274530e21f09203fb0723ed1836ebd81baa4f129
Guangzhuan Mo <gwongzaanmok@gmail.com>
2025-03-11 17:19:09 -0400

    ADD version.h for project version

---
+++ include/version.h
@@ -0,0 +1,19 @@
+/**
+ * @file version.h
+ * @author Guangzhuan Mo (gwongzaanmok@gmail.com)
+ * @brief Version definition
+ * @version 0.1.0
+ * @date 2025-03-09
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+#ifndef _NGHUNG_VERSION_H
+#define _NGHUNG_VERSION_H
+namespace NGHUNG
+{
+    /// @brief project version
+    constexpr char version[] = "0.1.0";
+} // namespace NGHUNG
+
+#endif
